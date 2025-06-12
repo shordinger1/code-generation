f017610647b1c1b21343e245b4f46e91dc57acb0
Your Name <you@example.com>
2025-06-12 10:37:37 +0800

    add openaiProcessor

--- task_tree/TaskProcessor.py
+++ task_tree/TaskProcessor.py
@@ -1,3 +1,5 @@
+from openai import OpenAI
+
 class TaskProcessor:
 
     def __init__(self):
@@ -6,4 +8,21 @@ class TaskProcessor:
     def process(self, **kwargs):
         raise NotImplementedError()
 
+class openaiProcessor:
 
+    def __init__(self):
+        api_key = 'sk-BGsbjdC7lXQGHgJyh61xf04bBeR1P6uFForzHoXU4jsmWsrx'
+        self.client = OpenAI(
+            api_key=api_key,
+            base_url="https://api.chatanywhere.tech/v1"
+        )
+
+    def process(self, prompt, generation_structure, model_type="gpt-4o-mini"):
+        completion = self.client.beta.chat.completions.parse(
+            model=model_type,
+            messages=[
+                {"role": "user", "content": prompt}
+            ],
+            response_format=generation_structure
+        )
+        return completion.choices[0].message.parsed
